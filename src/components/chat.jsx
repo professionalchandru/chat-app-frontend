de79b274a0799e2a9505fe2cc59fa19a428f82e1
professionalchandru <professionalchandru@gmail.com>
2022-04-14 15:45:51 +0530

    styles added for desktop background

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -40,15 +40,17 @@ const Chat = ({socket, room, userName}) => {
   return (
     <>
 
-        <div className='w-11/12 xl:w-1/2 py-5 mx-auto text-center text-black font-meidum text-xl'>
+        {/* <div className='w-11/12 xl:w-1/2 py-5 mx-auto text-center flex flex-row items-center justify-between text-black font-meidum text-xl space-x-5'>
             <h3>Current User: <span className='font-semibold'>{userName}</span></h3>
-        </div>
+            <h3>Room Id: <span className='font-semibold'>{room}</span></h3>
+        </div> */}
 
-        <div className='relative w-11/12 xl:w-1/2 h-50vh mx-auto border border-blue-100 rounded-md'>
+        <div className='relative w-11/12 xl:w-1/4   h-70vh mx-auto bg-white border border-border rounded-3xl'>
 
             {/* Chat Header */}
-            <div id='chat-header' className='bg-blue-500 rounded py-3'>
-                <p className='text-center text-white text-2xl font-medium'>Live Chat - {room}</p>
+            <div id='chat-header' className='w-full break-all bg-primary px-10 text-white rounded-t-3xl py-5 flex flex-row items-start justify-between space-x-5'>
+                <h3 className='w-1/2 text-left'>User:  <span className='font-semibold'>{userName}</span></h3>
+                <h3 className='w-1/2 text-right'>Room:  <span className='font-semibold'>{room}</span></h3>
             </div>
 
             {/* Chat Body */}
@@ -56,14 +58,14 @@ const Chat = ({socket, room, userName}) => {
                 <ScrollToBottom className="w-full h-full overflow-y- overflow-x-hidden no-scroll">
                     {messageList.map((item, index) => {
                         return(
-                            <div key={index} className={`my-4 px-2.5 h-auto flex text-center ${userName !== item.author ? 'items-start justify-start' : 'items-end justify-end'}`} id={userName !== item.author ? "you" : "other"}>
+                            <div key={index} className={`my-6 px-2.5 h-auto flex  ${userName !== item.author ? 'items-start justify-start' : 'items-end justify-end'}`} id={userName !== item.author ? "you" : "other"}>
                                 <div >
-                                    <div className={`w-auto h-auto px-3 py-2 break-words rounded-xl text-white flex ${userName !== item.author ? 'ml-2 items-start bg-green-500 justify-start' : 'mr-2 items-end bg-violet-500 justify-end'} text-center`} style={{maxWidth: '450px'}}>
+                                    <div className={`w-auto h-auto px-5 py-3  break-all rounded-3xl flex ${userName !== item.author ? 'ml-2 items-start bg-pink-600 justify-start text-white' : 'mr-2 items-end bg-primary text-white justify-end'}`} style={{maxWidth: '250px'}}>
                                         <p>{item.message}</p>
                                     </div>
                                     <div className={`px-3 py-1 flex flex-row items-center space-x-3 ${userName !== item.author ? 'ml-2 items-start justify-start' : 'mr-2 items-end justify-end'}`}>
-                                        <p className='text-xs text-black font-normal' >{item.time}</p>
-                                        <p className='text-xs text-black font-bold' >{item.author}</p>
+                                        <p className='text-xs text-font_primary font-medium' >{item.time}</p>
+                                        <p className='text-xs text-font_primary font-bold' >{item.author}</p>
                                     </div>
                                 </div>
                             </div>
@@ -73,19 +75,20 @@ const Chat = ({socket, room, userName}) => {
             </div>
 
             {/* Chat Footer */}
-            <div id='chat-footer' className='absolute px-1 bottom-2 w-full flex flex-row space-x-2 items-center bg-white'>
+            <div id='chat-footer' className='absolute pt-1 bottom-3 w-full flex flex-row space-x-2 items-center  bg-white border-t border-border'>
                 <div className='w-full'>
 
-                <Input 
+                <input 
                     type="text"
                     placeholder="Enter Message Here..."
                     value={currentMessage}
                     onChange={(e) => setCurrentMessage(e.target.value)}
-                    classes="border border-blue-100 w-full"
+                    className={`px-5 py-2 text-font_primary focus:outline-none rounded-md w-full`}
                     onKeyPress={(e) => e.key === "Enter" && sendMessage() }
                 />
                 </div>
-                <button onClick={()=> sendMessage()} className='px-5 py-2 rounded-full bg-indigo-500 text-white'>&#9658;</button>
+                
+                <img onClick={()=> sendMessage()} className="cursor-pointer" src={window.location.origin + "/assets/send.png"} alt="send icon" />
             </div>
 
         </div>
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -11,7 +11,7 @@ const Input = ({type, placeholder, value, onChange, required, disabled, classes,
             required={required}
             disabled={disabled}
             onKeyUp={onKeyPress}
-            className={`px-5 py-2 bg-gray-50 focus:bg-white border border-border focus:outline-none focus:ring-2 focus:ring-primary rounded-md ${classes}`}
+            className={`px-5 py-2 text-font_primary bg-gray-50 focus:bg-white border border-border focus:outline-none focus:ring-2 focus:ring-primary rounded-md ${classes}`}
         />
     </div>
   )
